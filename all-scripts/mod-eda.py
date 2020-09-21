b02bb2ce4e13526f957299a1cbe9a05932293933
eudesedu <eudes.edu@gmail.com>
2020-09-20 23:14:58 -0300

    develop obv function.

--- all-scripts/mod-eda.py
+++ all-scripts/mod-eda.py
@@ -88,44 +88,54 @@ def f_exploratory_data_index(set_wd, file_load, file_pattern, tickers):
                 ticker = ticker.rename(columns={'Date': 'DATA', 'Adj Close': tickers[index]+'_FECHAMENTO', 'Volume': tickers[index]+'_VOLUME'})
                 fi_cnpj = fi_cnpj.merge(ticker, left_on='DATA', right_on='DATA')
             # Relatório das análises exploratórias de dados.
-            fi_cnpj.to_csv('cnpj_'+file_pattern[step]+'_'+re.sub(regex_punctuation, "", cnpj_list[cnpj])+'.csv', sep=';', index=False, encoding='utf-8-sig')
-            # fi_profile = ProfileReport(fi_cnpj, title='Profiling Report')
-            # fi_profile.to_file(set_wd[2]+'\\temp\\cnpj_'+file_pattern[step]+'_'+re.sub(regex_punctuation, "", cnpj_list[cnpj])+'.html')
+            fi_cnpj.to_csv(file_load[2]+'_'+file_pattern[step]+'_'+re.sub(regex_punctuation, "", cnpj_list[cnpj])+'.csv', sep=';', index=False, encoding='utf-8-sig')
 
-def f_exploratory_data_obv(set_wd, file_load, file_pattern, tickers):
+def f_exploratory_data_obv(set_wd, file_load, file_pattern, tickers, volume_index, obv):
     """
     Gera os relatórios das análises exploratórias de dados para cada base de dados considerando o On-Balance Volume (OBV).
     """
     Client()
     os.chdir(set_wd[2])
-    # Define as variáveis do cadastro dos fundos de investimentos.
-    var_list = ['CNPJ', 'DATA', 'QUOTA', 'PATRIMONIO', 'COTISTAS', 'NOME', 'CLASSE', 'CONDICAO', 'COTAS', 'EXCLUSIVO', 'QUALIFICADO', '^BVSP_FECHAMENTO',
-                '^BVSP_VOLUME', '^GSPC_FECHAMENTO', '^GSPC_VOLUME', '^IXIC_FECHAMENTO', '^IXIC_VOLUME', '^TNX_FECHAMENTO', '000001.SS_FECHAMENTO',
-                '000001.SS_VOLUME', '^N225_FECHAMENTO', '^N225_VOLUME', '^VIX_FECHAMENTO', 'BRL=X_FECHAMENTO', 'GC=F_FECHAMENTO', 'GC=F_VOLUME',
-                'CL=F_FECHAMENTO', 'CL=F_VOLUME']
-    # Cria uma lista com os names dos arquivos com extenção CSV.
-    files_list = glob.glob('*cnpj*')
-    # Lê cada arquivo da lista removendo as variáveis desnecessárias:
-    for files in range(0, len(files_list)):
-        fi_cnpj = dd.read_csv(files_list[files], sep=';', engine='python', usecols=var_list, encoding='utf-8-sig')
-        fi_cnpj = fi_cnpj.compute()
-        fi_cnpj = fi_cnpj.rename(columns={'^BVSP_FECHAMENTO' : 'IBOV', '^BVSP_VOLUME' : 'IBOV_VOLUME', '^GSPC_FECHAMENTO' : 'SP500', 
-                                          '^GSPC_VOLUME' : 'SP500_VOLUME', '^IXIC_FECHAMENTO' : 'NASDAQ', '^IXIC_VOLUME' : 'NASDAQ_VOLUME',
-                                          '^TNX_FECHAMENTO' : 'BOND10Y', '000001.SS_FECHAMENTO' : 'SHANGHAI', '000001.SS_VOLUME' : 'SHANGHAI_VOLUME', 
-                                          '^N225_FECHAMENTO' : 'NIKKEI', '^N225_VOLUME' : 'NIKKEI_VOLUME', '^VIX_FECHAMENTO' : 'VOLATILITY', 
-                                          'BRL=X_FECHAMENTO' : 'DOLLAR', 'GC=F_FECHAMENTO' : 'GOLD', 'GC=F_VOLUME' : 'GOLD_VOLUME', 'CL=F_FECHAMENTO' : 'OIL', 
-                                          'CL=F_VOLUME' : 'OIL_VOLUME'})
-        volume = fi_cnpj.IBOV_VOLUME
-        list_obv = []
-        for i in range(0, len(volume) - 1):
-            previous_obv = volume.iloc[len(volume) - (len(volume) - i)]
-            current_obv = volume.iloc[len(volume) - (len(volume) - (i + 1))]
-            if current_obv == previous_obv:
-                list_obv.append(current_obv)
-            elif current_obv > previous_obv:
-                list_obv.append(previous_obv + current_obv)
-            elif current_obv < previous_obv:
-                list_obv.append(previous_obv - current_obv)
+    for step in range(0, 4):
+        # Define as variáveis do cadastro dos fundos de investimentos.
+        var_list = ['CNPJ', 'DATA', 'QUOTA', 'PATRIMONIO', 'COTISTAS', 'NOME', 'CLASSE', 'CONDICAO', 'COTAS', 'EXCLUSIVO', 'QUALIFICADO', '^BVSP_FECHAMENTO',
+                    '^BVSP_VOLUME', '^GSPC_FECHAMENTO', '^GSPC_VOLUME', '^IXIC_FECHAMENTO', '^IXIC_VOLUME', '^TNX_FECHAMENTO', '000001.SS_FECHAMENTO',
+                    '000001.SS_VOLUME', '^N225_FECHAMENTO', '^N225_VOLUME', '^VIX_FECHAMENTO', 'BRL=X_FECHAMENTO', 'GC=F_FECHAMENTO', 'GC=F_VOLUME',
+                    'CL=F_FECHAMENTO', 'CL=F_VOLUME']
+        # Cria uma lista com os names dos arquivos com extenção CSV.
+        files_list = glob.glob('*'+file_load[2]+'_'+file_pattern[step]+'*')
+        # Lê cada arquivo da lista removendo as variáveis desnecessárias:
+        for files in range(0, len(files_list)):
+            fi_cnpj = dd.read_csv(files_list[files], sep=';', engine='python', usecols=var_list, encoding='utf-8-sig')
+            fi_cnpj = fi_cnpj.compute()
+            fi_cnpj = fi_cnpj.rename(columns={'^BVSP_FECHAMENTO' : 'IBOV', '^BVSP_VOLUME' : 'IBOV_VOLUME', '^GSPC_FECHAMENTO' : 'SP500', 
+                                              '^GSPC_VOLUME' : 'SP500_VOLUME', '^IXIC_FECHAMENTO' : 'NASDAQ', '^IXIC_VOLUME' : 'NASDAQ_VOLUME',
+                                              '^TNX_FECHAMENTO' : 'BOND10Y', '000001.SS_FECHAMENTO' : 'SHANGHAI', '000001.SS_VOLUME' : 'SHANGHAI_VOLUME', 
+                                              '^N225_FECHAMENTO' : 'NIKKEI', '^N225_VOLUME' : 'NIKKEI_VOLUME', '^VIX_FECHAMENTO' : 'VOLATILITY', 
+                                              'BRL=X_FECHAMENTO' : 'DOLLAR', 'GC=F_FECHAMENTO' : 'GOLD', 'GC=F_VOLUME' : 'GOLD_VOLUME', 'CL=F_FECHAMENTO' : 'OIL', 
+                                              'CL=F_VOLUME' : 'OIL_VOLUME'})
+            for volume_index_count in range(0, len(volume_index)):
+                volume = fi_cnpj.loc[:, [volume_index[volume_index_count]]]
+                volume = volume[volume_index[volume_index_count]].tolist()
+                obv_list = []
+                for volume_count in range(0, len(volume) - 1):
+                    previous_volume_index = volume[volume_count]
+                    current_volume_index = volume[volume_count + 1]
+                    if current_volume_index == previous_volume_index:
+                        obv_list.append(current_volume_index)
+                    elif current_volume_index > previous_volume_index:
+                        obv_list.append(previous_volume_index + current_volume_index)
+                    elif current_volume_index < previous_volume_index:
+                        obv_list.append(previous_volume_index - current_volume_index)
+                obv_list.append(0)
+                obv_data = pd.DataFrame(obv_list)
+                obv_data = obv_data.rename(columns={0 : obv[volume_index_count]})
+                fi_cnpj = pd.concat([fi_cnpj, obv_data], axis=1)
+            fi_cnpj.drop(fi_cnpj.tail(1).index, inplace=True)
+            # Relatório das análises exploratórias de dados.
+            fi_cnpj.to_csv('obv_'+files_list[files], sep=';', index=False, encoding='utf-8-sig')
+            fi_profile = ProfileReport(fi_cnpj, title='Profiling Report')
+            fi_profile.to_file('obv_'+files_list[files][0:28]+'.html')
 
 def f_main():
     """
@@ -145,13 +155,15 @@ def f_main():
     file_load = ['fi_cad', 'fi_diario', 'cnpj']
     file_pattern = ['2017', '2018', '2019', '2020', 'inf']
     tickers = ['^BVSP', '^GSPC', '^IXIC', '^TNX', '000001.SS', '^N225', '^VIX', 'BRL=X', 'GC=F', 'CL=F']
+    volume_index = ['IBOV_VOLUME', 'SP500_VOLUME', 'NASDAQ_VOLUME', 'SHANGHAI_VOLUME', 'NIKKEI_VOLUME', 'GOLD_VOLUME', 'OIL_VOLUME']
+    obv = ['IBOV_OBV', 'SP500_OBV', 'NASDAQ_OBV', 'SHANGHAI_OBV', 'NIKKEI_OBV', 'GOLD_OBV', 'OIL_OBV']
     # Define os argumentos e variáveis como parâmetros de entrada para funções.
     if cmd_args.exploratory_data_cvm:
         f_exploratory_data_cvm(set_wd, file_load, file_pattern, tickers)
     if cmd_args.exploratory_data_index:
         f_exploratory_data_index(set_wd, file_load, file_pattern, tickers)
     if cmd_args.exploratory_data_obv:
-        f_exploratory_data_obv(set_wd, file_load, file_pattern, tickers)
+        f_exploratory_data_obv(set_wd, file_load, file_pattern, tickers, volume_index, obv)
 
 if __name__ == '__main__':
     f_main()
